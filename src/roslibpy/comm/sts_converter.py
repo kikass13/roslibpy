3980d0c41a890750994b2725aaa4790e29f16aeb
Sebastian Müller <sebastian.mueller2@streetscooter.eu>
2019-10-10 11:45:12 +0200

    Update sts_converter.py

--- src/roslibpy/comm/sts_converter.py
+++ src/roslibpy/comm/sts_converter.py
@@ -3,18 +3,26 @@
 
 class ConversionHelper:
     @staticmethod
-    def grabAttrs(d):
-        values = {}
-        for k,v in d.items():
-            newAttr = ConversionHelper.grabAttr(k, v)
-            values[k] = newAttr
-        return values
+    def grabAttrs(container):
+	    values = None
+	    if(isinstance(container, dict)):
+	    	values = {}
+	    	for k,v in container.items():
+				newAttr = CrawlerHelper.grabAttr(k, v)
+				values[k] = newAttr
+		return values
+
     @staticmethod
     def grabAttr(k, v):
         attr = None
         if(isinstance(v, dict)):
             attrs = ConversionHelper.grabAttrs(v)
             attr = AttributeHolder(attrs)
+        elif(isinstance(v, list)):
+            tmp = []
+            for value in v:
+                tmp.append(CrawlerHelper.grabAttr(None,value))
+            attr = AttributeList(k,tmp)
         else:
             attr = Attribute(k,v)
         return attr
@@ -27,6 +35,10 @@ class AttributeHolder(object):
     def __repr__(self):
         return str(self.__dict__)
 
+class AttributeList(list):
+    def __init__(self, k,v):
+        super(AttributeList, self).__init__(v)
+
 class Attribute(object):
     def __init__(self, k,v):
         super(Attribute, self).__init__()
@@ -34,11 +46,6 @@ class Attribute(object):
         self.v = v
     def __repr__(self):
         return str(self.v)
-    def __iter__(self):
-        return iter(self.v)
-    def __next__(self):
-        next(self.v)
-
 
 class JsonConverter(object):
     def __init__(self, d):
